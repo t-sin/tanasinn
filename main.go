0625d58772127e8a0be69e205e55d75c8beab1b6
t-sin <shinichi.tanaka45@gmail.com>
2020-03-02 17:35:02 +0900

    Threashold varies in progress

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -19,11 +20,47 @@ func prinHelp() {
 type option struct {
 	input * os.File
 	isHelp bool
-	th float32
+	th []float64
 }
 
+type parseError struct {
+	s string
+	reason string
+}
 
-func tanasinnize(th float32, r rune) string {
+func (e *parseError) Error() string {
+	return fmt.Sprintf("cannot parse %s because %s", e.s, e.reason)
+}
+
+func parseThreasholds(th string) ([]float64, error) {
+	var ths []float64
+
+	if strings.Contains(th, ",") {
+		list := strings.Split(th, ",")
+		for i := 0; i < len(list); i++ {
+
+			f, err := strconv.ParseFloat(list[i], 32)
+			if err != nil {
+				log.Fatal(err)
+				return ths, &parseError{s: th, reason: "not float in the list"}
+			}
+
+			ths = append(ths, f)
+		}
+
+	} else {
+		f, err := strconv.ParseFloat(th, 32)
+		if err != nil {
+			log.Fatal(err)
+			return []float64{}, &parseError{s: th, reason: "not float"}
+		}
+		ths = []float64{f}
+	}
+
+	return ths, nil
+}
+
+func tanasinnize(th float64, r rune) string {
 	s := []string{
 		"∴",
 		"∵",
@@ -33,11 +70,11 @@ func tanasinnize(th float32, r rune) string {
 		"(･",
 		"∴･",
 		"∴",
-        "･･",
-        "･:",
-        "..",
+		"･･",
+		"･:",
+		"..",
 	}
-	if th < rand.Float32() {
+	if th < rand.Float64() {
 		return fmt.Sprintf("%s", s[int(rand.Uint32()) % len(s)])
 	} else {
 		return fmt.Sprintf("%c", r)
@@ -45,7 +82,7 @@ func tanasinnize(th float32, r rune) string {
 }
 
 func main() {
-	var option = option{input: os.Stdin, th: 0.7, isHelp: false}
+	var option = option{input: os.Stdin, th: []float64{0.8}, isHelp: false}
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	for i := 1; i < len(os.Args); i++ {
@@ -61,12 +98,12 @@ func main() {
 					option.isHelp = true
 				case 't':
 					i++
-					f, err := strconv.ParseFloat(os.Args[i], 32)
+					ths, err := parseThreasholds(os.Args[i])
 					if err != nil {
 						log.Fatal(err)
 						os.Exit(1)
 					}
-					option.th = float32(f)
+					option.th = ths
 				}
 			}
 
@@ -86,11 +123,23 @@ func main() {
 	}
 
 	reader := bufio.NewReader(option.input)
+	var text []rune = []rune{}
 	for true {
 		r, _, err := reader.ReadRune()
 		if err == io.EOF {
 			break
 		}
-		fmt.Printf("%s", tanasinnize(option.th, r))
+		text = append(text, r)
+	}
+
+	for n := 0; n < len(text); n++ {
+		p := float64(n) / float64(len(text))
+		i := int(p * float64(len(option.th)))
+		r := text[n]
+		if r == '\n' {
+			fmt.Printf("\n")
+		} else {
+			fmt.Printf("%s", tanasinnize(option.th[i], r))
+		}
 	}
 }
