0e17d795539e08d61d813a5001388c77e7d279a2
t-sin <shinichi.tanaka45@gmail.com>
2020-03-02 15:25:40 +0900

    Basic CLI

--- main.go
+++ main.go
@@ -2,9 +2,46 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
-func main() {
+func prinHelp() {
 	fmt.Println("tanasinn...")
+	fmt.Println("usage: tanasin [OPTION] [FILE]")
+}
+
+type option struct {
+	input * os.File
+	isHelp bool
+}
+
+func main() {
+	var option option
+
+	for i := 1; i < len(os.Args); i++ {
+
+		if os.Args[i][0] == '-' {
+
+			if len(os.Args[i]) < 2 {
+				option.input = os.Stdin
+
+			} else {
+				switch os.Args[i][1] {
+				case 'h':
+					option.isHelp = true
+				}
+			}
+
+		} else if option.input == nil {
+			f, err := os.OpenFile(os.Args[i], os.O_RDONLY, 0x600)
+			if err != nil {
+				fmt.Printf("cannot open `%s`\n", os.Args[i])
+				os.Exit(1)
+			}
+			option.input = f
+		}
+	}
+
+	fmt.Println(option)
 }
 
